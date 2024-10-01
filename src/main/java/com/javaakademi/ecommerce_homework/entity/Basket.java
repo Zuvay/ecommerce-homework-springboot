a1d7d8f26366b0fb60e9fb5712e0baaf28c7f360
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-10-01 23:51:44 +0300

    sepet için yeni eklenen entity bağlantısı yapıldı

--- src/main/java/com/javaakademi/ecommerce_homework/entity/Basket.java
+++ src/main/java/com/javaakademi/ecommerce_homework/entity/Basket.java
@@ -9,27 +9,10 @@ public class Basket {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @OneToMany
-    private List<Product> products;
-
-    public Basket(int id, List<Product> products) {
-        this.id = id;
-        this.products=products;
-    }
-    public Basket(){}
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
+    @ManyToOne
+    private User user_id;
+    @OneToOne
+    private List<BasketProduct> basketProducts;
+    
+    //getter setter ve const eklenecek
 }
